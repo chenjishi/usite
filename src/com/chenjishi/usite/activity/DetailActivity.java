76cee0bb2eb9bbad874b7fb7bc30a73c4bbb5b03
chenjishi <chenjishi@gmail.com>
2012-11-07 18:21:19 +0800

    back icon added

--- src/com/chenjishi/usite/activity/DetailActivity.java
+++ src/com/chenjishi/usite/activity/DetailActivity.java
@@ -1,7 +1,9 @@
 package com.chenjishi.usite.activity;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
@@ -18,49 +20,47 @@ import com.chenjishi.usite.util.ApiUtils;
  * Time: 下午7:56
  * To change this template use File | Settings | File Templates.
  */
-public class DetailActivity extends Activity {
+public class DetailActivity extends Activity implements View.OnClickListener {
     private WebView mWebView;
-
     private JSInterface mJsInterface;
-
-    private String mUrl;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detail);
 
+
+        String url = ApiUtils.BASE_URL + getIntent().getExtras().getString("link");
+        
+        findViewById(R.id.icon_back).setOnClickListener(this);
+
         mWebView = (WebView) findViewById(R.id.content_webview);
-
-        mUrl = ApiUtils.BASE_URL + getIntent().getExtras().getString("link");
-
         mWebView.getSettings().setJavaScriptEnabled(true);
-
         mJsInterface = new JSInterface();
-
         mWebView.addJavascriptInterface(mJsInterface, "jsobj");
         mWebView.loadUrl("file:///android_asset/test.html");
-
         mWebView.setWebChromeClient(new WebChromeClient() {
             @Override
             public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
-                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();;
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
                 return true;
             }
         });
+        new LoadPageTask().execute(url);
+    }
 
-        new Thread() {
-            @Override
-            public void run() {
-                Article result = ArticleParser.getContent(mUrl);
-                mJsInterface.setContent(result);
+    class LoadPageTask extends AsyncTask<String, Void, Article> {
+        @Override
+        protected Article doInBackground(String... strings) {
+            return ArticleParser.getContent(strings[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Article article) {
+            if (null != article) {
+                mJsInterface.setContent(article);
             }
-        }.start();
-
-
-
+        }
     }
 
     class JSInterface {
@@ -68,8 +68,15 @@ public class DetailActivity extends Activity {
             String p1 = "{'title':\'" + article.title + "\'}";
             String p2 = "{'content':\'" + article.content + "\'}";
             mWebView.loadUrl("javascript:setArticle(" + p1 + ", " + p2 + ")");
-
         }
+    }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.icon_back:
+                finish();
+                break;
+        }
     }
 }
--- src/com/chenjishi/usite/parser/ArticleParser.java
+++ src/com/chenjishi/usite/parser/ArticleParser.java
@@ -17,10 +17,12 @@ import java.io.IOException;
  */
 public class ArticleParser {
     public static Article getContent(String url) {
-        Article article = new Article();
+        Article article = null;
 
         try {
             Document doc = Jsoup.connect(url).get();
+
+            article = new Article();
 
             Elements title = doc.getElementsByClass("link2");
             article.title = title.get(0).text();
@@ -28,57 +30,13 @@ public class ArticleParser {
             Elements content = doc.getElementsByClass("content");
 
             Elements tags = content.get(0).getAllElements();
-
-//            Elements links = content.get(0).getElementsByTag("p");
-//
             StringBuilder sb = new StringBuilder();
-//            for (Element link : links) {
-//                for (Element img : link.getElementsByTag("img")) {
-//                    String imgUrl = img.attr("src");
-//                    if (imgUrl.length() > 0) {
-//                        sb.append("<img src=" + imgUrl + " />");
-//                    }
-//                }
-//
-//                for (Element stg : link.getElementsByTag("strong")) {
-//                    String _stg = stg.text();
-//                    if (_stg.length() > 0) {
-//                        sb.append("<strong>" + _stg + "</strong><br />");
-//                    }
-//                }
-//
-//                for (Node node : link.childNodes()) {
-//                    if (node instanceof TextNode) {
-//                        String s = ((TextNode) node).text();
-//                        if (s.length() > 0) {
-//                            sb.append(s + "<br /><br />");
-//                        }
-//                    }
-//
-//                }
-//            }
 
             for (Element tag : tags) {
-//                if (tag.tagName().equalsIgnoreCase("p")) {
-//                    Elements brs = tag.getAllElements();
-//                    for (Element el : brs) {
-//                        Log.i("test", "inside p " + el.tagName() + " inside p " + el.text());
-//                    }
-//
-////                    String s = tag.text().trim();
-////                    Log.i("test", "p =====> " + s.length() + " text ---> " + s);
-//                }
-
-//                if (tag.tagName().equalsIgnoreCase("img")) {
-//                    Log.i("test", "img =====> " + tag.attr("src"));
-//                }
-
                 if (tag.tagName().equalsIgnoreCase("p")) {
-//                    sb.append("<p>" + tag.html() + "<br /></p>");
                     sb.append(tag.html() + "<br />");
                 }
             }
-
             article.content = sb.toString();
         } catch (IOException e) {
             e.printStackTrace();
