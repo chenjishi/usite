f628e3cf057a16bdd631d9c3889a11326b085bc5
jishi chen <chenjishi313@gmail.com>
2017-04-24 15:43:18 +0800

    deleted old project

--- U148/app/src/main/java/com/chenjishi/u148/utils/ErrorListener.java
+++
@@ -1,9 +0,0 @@
-package com.chenjishi.u148.utils;
-
-/**
- * Created by chenjishi on 16/9/5.
- */
-public interface ErrorListener {
-
-    void onErrorResponse();
-}
--- U148/app/src/main/java/com/chenjishi/u148/utils/OnPageEndListener.java
+++
@@ -1,9 +0,0 @@
-package com.chenjishi.u148.utils;
-
-/**
- * Created by chenjishi on 16/2/3.
- */
-public interface OnPageEndListener {
-
-    void onPageEnd();
-}
